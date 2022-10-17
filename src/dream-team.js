a78760d2b830108d6b56b3c3091bf9c52dce5b1a
Ilya <82887965+partymaker19@users.noreply.github.com>
2022-10-18 00:48:41 +0300

    feat: basic-js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,32 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let name = [];
+
+  for (let elem of Object.values(members)) {
+    if (typeof elem == "string") {
+      elem = elem.split(" ").join("");
+      name.push(elem[0].toUpperCase());
+    }
+  }
+
+  name = name.sort();
+  return name.join("");
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,49 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  let season = [
+    "winter",
+    "spring",
+    "summer",
+    "fall",
+    "Unable to determine the time of year!",
+  ];
+
+  if (!date) {
+    return season[4];
+  }
+
+  if (date instanceof Date) {
+    if (typeof date.getMonth === "function" && date.getMonth) {
+      let month = date.getMonth();
+      if (month === 0 || manth === 1 || month === 11) {
+        return season[0];
+      } else if (month === 2 || month === 3 || month === 4) {
+        return season[1];
+      } else if (month === 5 || month === 6 || month === 7) {
+        return season[2];
+      } else if (month === 8 || month === 9 || month === 10) {
+        return season[3];
+      }
+    } else {
+      throw new Error("Invalid date!");
+    }
+  } else {
+    throw new Error("Invalid date!");
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
