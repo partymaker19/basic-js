c89f625e07e0c5f368b7eb8d272aa3306390f380
Ilya <82887965+partymaker19@users.noreply.github.com>
2023-04-17 19:48:49 +0300

    feat: basic-js

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,7 +16,19 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  * The result should be 9
  */
-function getMatrixElementsSum(matrix) {}
+function getMatrixElementsSum(matrix) {
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        count += matrix[i][j];
+      } else if (matrix[i - 1][j] !== 0) {
+        count += matrix[i][j];
+      }
+    }
+  }
+  return count;
+}
 
 module.exports = {
   getMatrixElementsSum,
